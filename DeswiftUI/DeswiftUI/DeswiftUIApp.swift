d68a1f068b6edd40216198b207ad9f67cec19f0a
J-C-1020 <80874022+J-C-1020@users.noreply.github.com>
2025-11-12 10:30:45 +0100

    Add initialization of project in Xcode

---
+++ DeswiftUI/DeswiftUI/DeswiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeswiftUIApp.swift
+//  DeswiftUI
+//
+//  Created by Yolanda Cantu on 12/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DeswiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
