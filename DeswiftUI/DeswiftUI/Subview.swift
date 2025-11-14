8d6575f41cd06a4968a7c4d53eaa05f502e0a7d1
J-C-1020 <80874022+J-C-1020@users.noreply.github.com>
2025-11-14 11:45:07 +0100

    Main template view that will be modified copying Apple's apple music app - album view, & declaration of model for modifiers of the ui elements

---
+++ DeswiftUI/DeswiftUI/Subview.swift
@@ -0,0 +1,24 @@
+//
+//  PodcastSubview.swift
+//  DeswiftUI
+//
+//  Created by Yolanda Cantu on 12/11/25.
+//
+
+import SwiftUI
+
+struct Subview: View {
+    var body: some View {
+        VStack{
+            HStack {
+                Image("duck")
+            }.padding()
+            Spacer()
+            
+        }
+    }
+}
+
+#Preview {
+    Subview()
+}
