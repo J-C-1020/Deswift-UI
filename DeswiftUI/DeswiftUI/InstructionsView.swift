f32b3988701406634c2c06778f36ad6ba7c6d8e5
J-C-1020 <80874022+J-C-1020@users.noreply.github.com>
2025-11-16 23:57:32 +0100

    Main menu and instructions view

---
+++ DeswiftUI/DeswiftUI/InstructionsView.swift
@@ -0,0 +1,18 @@
+//
+//  InstructionsView.swift
+//  DeswiftUI
+//
+//  Created by Yolanda Cantu on 16/11/25.
+//
+
+import SwiftUI
+
+struct InstructionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InstructionsView()
+}
