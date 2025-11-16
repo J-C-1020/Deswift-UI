812f1586f58d60db0597b21607b7a6ffceaaebfb
J-C-1020 <80874022+J-C-1020@users.noreply.github.com>
2025-11-16 21:32:08 +0100

    icon editor view update

---
+++ DeswiftUI/DeswiftUI/DesignEditor.swift
@@ -0,0 +1,18 @@
+//
+//  DesignEditor.swift
+//  DeswiftUI
+//
+//  Created by Yolanda Cantu on 16/11/25.
+//
+
+import SwiftUI
+
+struct DesignEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DesignEditor()
+}
